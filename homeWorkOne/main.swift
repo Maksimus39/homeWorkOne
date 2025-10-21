5af9ed6fbbe406d4386cab8ac0d93c4b9a5e87be
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-20 21:44:06 -0700

    lesson 3 array

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,160 +1,150 @@
-// ------------------ Concatenation --------------
+// ------------------------------- optional types -------------
 
 // task_1
 
-let firstName:String = "Максим"
-let lastName:String = "Минаков"
-let fullName:String = firstName + " " + lastName
-
-print("fullName -> \(fullName)")
-
+var name: String? = nil
+name = "Максим"
+print(name)
 
 
 // task_2
 
-let age:UInt8 = 42
+var age: Int? = nil
 
-let resultMessage = "Меня зовут " + firstName + ", и мне " + String(age) + " года."
-print("resultMessage -> \(resultMessage)")
-
-
-// task 3
-
-print("Введите первое число.")
-let firstNumber = Int(readLine()!)!
-
-print("Введите второе число.")
-let secondNumber = Int(readLine()!)!
-
-let sumResult = firstNumber + secondNumber
-let sumFirstandSecondNumber = "Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(sumResult) + "."
-print("sumFirstandSecondNumber -> \(sumFirstandSecondNumber)")
-
-
-
-
-// --------------------- string interpolation ---------------
-
-// task_1
-
-let ageTestIntorpolation:UInt8 = 42
-
-let resultMessageInterpolation = "Меня зовут \(firstName), и мне \(age) года."
-print("resultMessageInterpolation -> \(resultMessageInterpolation)")
-
-
-// task_2
-
-print("Введите ваш вес в кг:")
-let weight = Double(readLine()!)!
-
-print("Введите ваш рост в см:")
-let heightCm = Double(readLine()!)!
-
-let heightM = heightCm / 100
-
-let bmi = weight / (heightM * heightM)
-
-print("Ваш ИМТ равен: \(bmi)")
-
-
-
-// task_3
-
-let productName:String = "MacBooK Air"
-let price:Int = 54_000
-let quantity:Int = 22
-
-let totalPrice = "Вы добавили в корзину \(quantity) шт. Товара \(productName) на сумму \(price * quantity) руб"
-
-print("totalPrice -> \(totalPrice)")
-
-
-
-// ----------------- main data types ------------
-
-// task_1
-
-var minIntSixTeen:Int16 = -32_768
-var numberUintEight:UInt8 = 200
-var numberDouble:Double = 3.1415926535
-var isBool:Bool = false
-let message:String = "Боятся нужно только Бога"
-
-
-
-// task_2
-
-print("Пользователь, введите целое число?")
-
-let inputNumber = Int(readLine()!)!
-
-let squareNumber = inputNumber * inputNumber
-print("Результат возведения числа в квадрат равен \(squareNumber)")
-
-
-// task_3
-
-let inputNumberTwo = 42
-let isEven:Bool
-
-isEven = inputNumberTwo % 2 == 0
-print("Введённое число является \(isEven) значением.")
-
-
-
-
-// --------------------- variable and constant -------------------------
-
-// task_1
-
-var temperature:Int8 = 7
-temperature = 10
-
-
-// task_2
-
-let birthYear:Int16 = 1983          // Выбрал данный тип так как было время и до нашей эры а более чем 32 века у нас значительный запас
-//birthYear = 1982                  // константа подразумевает объявление присвоенного значения в переменной единожды
-
-
-// task_3
-
-var count = 10
-//count = "Mobile IOS developer"
-
-/*
- Тут произошло следующее: я могу ошибаться но или компилятор
-  или интерпритатор по умолчанию задаст переменной тип при
-  объявлении без явного присвоения типа как type notation
-  а потом я вмешиваюсь и явно пытаюсь изменить не только значение
-  но и сам тип данных и язык это не допустит
- */
-
-
-
-// ---------------------- if/else ------------------
-
-// task_1
-
-let testNumber = 345_123
-
-if testNumber > 0 {
-    print("Число \(testNumber), является положительным")
-} else if testNumber < 0 {
-    print("Число \(testNumber), является отрицательным")
+if let userAge = age {
+    print(userAge)
 } else {
-    print("Число \(testNumber), является нулём")
+    print("Dозраст не указан")
+}
+
+
+// task_3
+
+var nickname: String? = "ErrorNil"
+
+if let errorNillNickName = nickname {
+    print("Твой ник: \(errorNillNickName)")
+} else {
+    print("Ника нет")
+}
+
+
+// task_4
+
+var email: String? = nil
+
+if let emailOptional = email {
+    print("Твой email: \(emailOptional)")
+} else {
+    print("email не задан")
+}
+
+
+// task_5
+
+var nameFive: String? = "Boris"
+var ageFive: Int? = 23
+var cityFive: String? = "Rom"
+
+print("Имя: \(nameFive ?? "Имя не указано")")
+print("Возраст: \(ageFive ?? 0 )")
+print("Город: \(cityFive ?? "Город не указан")")
+
+
+
+// task_6
+
+var score: Int? = 85
+
+if let userScore = score {
+    if userScore >= 80 {
+        print("Отлично")
+    } else if userScore >= 50 {
+        print("Хорошо")
+    } else {
+        print("Нужно подтянуть")
+    }
+} else {
+    print("Нет данных")
+}
+
+
+// task_7
+
+var numberString: String? = "42"
+
+if let numberStringInt = Int(numberString ?? "") {
+    print("Число: \(numberStringInt)")
+    print(type(of: numberStringInt))
+} else {
+    print("Некоректное значение")
+}
+
+
+// task_8
+
+var day = 10
+
+switch day {
+case 1: print("Понедельник")
+case 2: print("Вторник")
+case 3: print("Среда")
+default : print("День не распознан")
+}
+
+
+// task_9
+
+var color = "ыва"
+
+switch color {
+case "red": print("Стой")
+case "yellow": print("Жди")
+case "green": print("Иди")
+default: print("Неизвестный цвет")
+}
+
+
+// ----------------------------- Доп --------------------------
+
+// task_1
+
+var numberStringTwo: String? = "123"
+
+if let numInt = Int(numberStringTwo ?? "") {
+    print("Число: \(numInt)")
+    print(type(of: numInt))
+} else {
+    print("Не удалось преобразовать")
 }
 
 
 
 // task_2
 
-let userAge = 42
+var userName: String? = "Alice"
+var password: String? = nil
 
-if userAge >= 18 {
-    print("Доступ разрешён!")
+
+if let name = userName , let _ = password {  // я тут опустил переменную т.к она не используется и горит предупреждение
+    print("Добро пожаловать, \(name)")
 } else {
-    print("Доступ запрещён!")
+    print("Введите имя и пароль")
 }
+
+
+// task_3
+
+var nameTwo: String? = "Bob"
+var emailTwo: String? = nil
+var country: String? = "Kazahstan"
+
+
+if let nameOptional = nameTwo, let emailOptionalTwo = emailTwo, let countryOptional = country {
+    print("Профиль: \(nameOptional), \(emailOptionalTwo), \(countryOptional)")
+} else {
+    print("Заполните все поля.")
+}
+
+
+
