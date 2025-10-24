4224bbb1d62f8dfc111a622eb4ab58a2c36f5199
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-24 15:04:31 -0700

    lesson_4

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,160 +1,130 @@
-// ------------------ Concatenation --------------
-
 // task_1
 
-let firstName:String = "Максим"
-let lastName:String = "Минаков"
-let fullName:String = firstName + " " + lastName
+var numberArray: [Int] = [10, 20, 30, 40, 50]
 
-print("fullName -> \(fullName)")
-
-
-
-// task_2
-
-let age:UInt8 = 42
-
-let resultMessage = "Меня зовут " + firstName + ", и мне " + String(age) + " года."
-print("resultMessage -> \(resultMessage)")
-
-
-// task 3
-
-print("Введите первое число.")
-let firstNumber = Int(readLine()!)!
-
-print("Введите второе число.")
-let secondNumber = Int(readLine()!)!
-
-let sumResult = firstNumber + secondNumber
-let sumFirstandSecondNumber = "Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(sumResult) + "."
-print("sumFirstandSecondNumber -> \(sumFirstandSecondNumber)")
-
-
-
-
-// --------------------- string interpolation ---------------
-
-// task_1
-
-let ageTestIntorpolation:UInt8 = 42
-
-let resultMessageInterpolation = "Меня зовут \(firstName), и мне \(age) года."
-print("resultMessageInterpolation -> \(resultMessageInterpolation)")
-
-
-// task_2
-
-print("Введите ваш вес в кг:")
-let weight = Double(readLine()!)!
-
-print("Введите ваш рост в см:")
-let heightCm = Double(readLine()!)!
-
-let heightM = heightCm / 100
-
-let bmi = weight / (heightM * heightM)
-
-print("Ваш ИМТ равен: \(bmi)")
-
-
-
-// task_3
-
-let productName:String = "MacBooK Air"
-let price:Int = 54_000
-let quantity:Int = 22
-
-let totalPrice = "Вы добавили в корзину \(quantity) шт. Товара \(productName) на сумму \(price * quantity) руб"
-
-print("totalPrice -> \(totalPrice)")
-
-
-
-// ----------------- main data types ------------
-
-// task_1
-
-var minIntSixTeen:Int16 = -32_768
-var numberUintEight:UInt8 = 200
-var numberDouble:Double = 3.1415926535
-var isBool:Bool = false
-let message:String = "Боятся нужно только Бога"
-
-
-
-// task_2
-
-print("Пользователь, введите целое число?")
-
-let inputNumber = Int(readLine()!)!
-
-let squareNumber = inputNumber * inputNumber
-print("Результат возведения числа в квадрат равен \(squareNumber)")
-
-
-// task_3
-
-let inputNumberTwo = 42
-let isEven:Bool
-
-isEven = inputNumberTwo % 2 == 0
-print("Введённое число является \(isEven) значением.")
-
-
-
-
-// --------------------- variable and constant -------------------------
-
-// task_1
-
-var temperature:Int8 = 7
-temperature = 10
-
-
-// task_2
-
-let birthYear:Int16 = 1983          // Выбрал данный тип так как было время и до нашей эры а более чем 32 века у нас значительный запас
-//birthYear = 1982                  // константа подразумевает объявление присвоенного значения в переменной единожды
-
-
-// task_3
-
-var count = 10
-//count = "Mobile IOS developer"
-
-/*
- Тут произошло следующее: я могу ошибаться но или компилятор
-  или интерпритатор по умолчанию задаст переменной тип при
-  объявлении без явного присвоения типа как type notation
-  а потом я вмешиваюсь и явно пытаюсь изменить не только значение
-  но и сам тип данных и язык это не допустит
- */
-
-
-
-// ---------------------- if/else ------------------
-
-// task_1
-
-let testNumber = 345_123
-
-if testNumber > 0 {
-    print("Число \(testNumber), является положительным")
-} else if testNumber < 0 {
-    print("Число \(testNumber), является отрицательным")
-} else {
-    print("Число \(testNumber), является нулём")
+for element in numberArray {
+    print(element)
 }
 
 
 
 // task_2
 
-let userAge = 42
+numberArray.append(60)
+numberArray.insert(5, at: 0)
+print(numberArray)
 
-if userAge >= 18 {
-    print("Доступ разрешён!")
-} else {
-    print("Доступ запрещён!")
+
+
+// task_3
+
+let lastElement = numberArray.popLast()
+if let lastElement {
+    print("lastElement -> \(lastElement)")
+}
+print(numberArray) // -> Да, мы видим что последний элемент удалён
+
+
+
+// task_4
+
+print(numberArray[0])
+print(numberArray[numberArray.count - 1])
+
+
+// task_5
+
+var sum = 0
+for number in numberArray {
+    sum += number
+    print(sum)
+}
+
+
+// task_6
+
+let isNumberArray = numberArray.contains(10)
+print(isNumberArray)                           // true
+
+
+
+// task_7
+
+print(numberArray.count)
+
+
+
+// task_8
+
+print("numberArray -> \(numberArray)")
+numberArray[2] = 999
+print("numberArray -> \(numberArray)")
+
+
+
+// -------------------------------------------- PARTH TWO ---------------------------
+
+
+// task_1
+
+let fruits = ["banana", "orange", "apple", "kiwi"]
+
+fruits.contains("apple")
+? print("YES")
+: print("NO")
+
+
+// task_2
+
+print(fruits.sorted(by: <))
+
+/*
+ 
+ sorted() возвращает новый массив, исходный не изменяется
+ Если нужна сортировка на месте, используйте sort() (без возвращаемого значения)
+ 
+ */
+
+
+
+// task_3
+
+let filterFruitsArray = fruits.filter{ $0.count > 5 }
+print("Фрукты длиной более 5ти символов -> \(filterFruitsArray)")
+
+
+
+// task_4
+
+var str = "Hello World!"
+
+let chars = Array(str.lowercased())
+let reversedChars = chars.reversed()
+
+print(chars == Array(reversedChars))
+
+
+
+// task_5
+
+let numArray = [1, 3, 2, 5, 6, 7, 8, 3, 4, 5, 6, 7, 9, 0, 1, 2, 3, 5, 7, 3]
+let countOfThree = numArray.filter{ $0 == 3 }.count
+print("Число 3 встречается в массиве \(countOfThree) раз")
+
+
+
+// task_6
+
+let filterArray = numArray.filter{ $0 > 5}
+print(filterArray)
+
+
+// task_7
+
+let arr = [1, 2, 4, 10, 8, 16]
+
+var temp = 0
+for number in arr {
+    temp += number
+    print(temp)
 }
