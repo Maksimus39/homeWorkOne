1e565780e9892e8ebd6d37e7fe8d45b99e50ae39
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-31 17:42:48 -0700

    homeWorkStruct

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,100 +1,74 @@
-// Struct -> value type
-// Память
+// task_1
 
-var age: Int
-
-func someFunc(){
-    let _: String = "User"
+struct Point {
+    let x: Int
+    let y: Int
 }
 
-someFunc()
+let dot: Point = Point(x: 5, y: 7)
+print("Координаты точки - x:\(dot.x) y:\(dot.y)")
 
-// struct -> является объектом
+
+// task_2
+
+struct Rectangle {
+    let width: Int
+    let height: Int
+    
+    func area() -> Int {
+        return width * height
+    }
+}
+
+let rect: Rectangle = Rectangle(width: 3, height: 4)
+let resultSquare = rect.area()
+print("resultSquare -> \(resultSquare)")
+
+
+// task_3
+
+struct Student {
+    let name: String
+    let grade: Int
+    
+    
+    func isBetter(than: Student) -> Bool {
+        return self.grade > than.grade
+    }
+}
+
+let studentIvan: Student = Student(name: "Ivan", grade: 92)
+let studentPetr: Student = Student(name: "Petr", grade: 91)
+
+print("Первый студент: \(studentIvan.name), и его оценка: \(studentIvan.grade)")
+print("Второй студент: \(studentPetr.name), и его оценка: \(studentPetr.grade)")
+
+
+if studentIvan.isBetter(than: studentPetr){
+    print("\(studentIvan.name) учится лучше чем \(studentPetr.name)")
+} else if studentPetr.isBetter(than: studentIvan){
+    print("\(studentPetr.name) учится лучше чем \(studentIvan.name)")
+} else {
+    print("Студенты учатся одинаково хорошо")
+}
+
+
+// task_4
 
 struct User {
-    // свойства
-    
     var name: String
-    var surname: String
-    var age: UInt8
-    var hp: Int
-    
-    // методы
-    
-    func run(speed: Float) {
-        print("User \(name) бежит со скоростью \(speed) км/ч")
-    }
-    
-    func fight(damage: Int) {
-        if hp < 90 {
-            print("Удар был вполсилы \(damage / 2)")
-        } else {
-            print("Удар был со всей силы \(damage)")
-        }
-    }
-    
-    func speak(str: String) -> [Character] {
-        var char: [Character] = []
-        for it in str{
-            char.append(it)
-        }
-        return char
-    }
-    
-    func getFullName() -> String {
-        return name + " " + surname
-    }
-    
-    mutating func setNewName(newName: String){
-        self.name = newName
-    }
+    var email:String?
 }
 
-// экземпляр User
-var admin: User = User(name: "Maksim", surname: "Minakov", age: 42, hp: 80)
-print("admin -> \(admin)")
-admin.run(speed: 10)
-admin.fight(damage: 10)
-let fullName =  admin.getFullName()
-print("fullName -> \(fullName)")
-admin.age = 43
-print("admin -> \(admin)")
-admin.setNewName(newName: "Max")
-print(admin)
+var userNoEmail: User = User(name: "Ivan")
+var userEmail: User = User(name: "Petr", email: "petr@example.com")
 
-
-// --------
-let manager: User = User(name: "Larisa", surname: "Suslova", age: 39, hp: 100)
-print("manager -> \(manager)")
-manager.run(speed: 11)
-manager.fight(damage: 10)
-let speak = manager.speak(str: "Hello")
-print("speak -> \(speak)")
-let fullNameLarisa = manager.getFullName()
-print("fullNameLarisa -> \(fullNameLarisa)")
-
-
-// -----------
-
-var users: [User] = [admin, manager]
-print("users -> \(users.count)")
-
-for user in users {
-    print(user.getFullName())
+if let email = userEmail.email {
+    print("Email: \(email)")
+} else {
+    print("Email не указан")
 }
 
+print("userNoEmail -> \(userNoEmail)")
 
 
-
-
-func someFunction() {
-    defer {
-        // Этот код выполнится перед выходом из функции
-        print("Выполняем cleanup")
-    }
-    
-    // Основной код функции
-    print("Выполняем основную работу")
-}
-
-someFunction()
