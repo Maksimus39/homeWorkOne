78eba272b2671b2bb370410434c2206e0de725dc
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-01 18:16:09 -0700

    repeatStruct wilset didset static

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,160 +1,195 @@
-// ------------------ Concatenation --------------
+import Foundation
 
-// task_1
 
-let firstName:String = "Максим"
-let lastName:String = "Минаков"
-let fullName:String = firstName + " " + lastName
 
-print("fullName -> \(fullName)")
-
-
-
-// task_2
-
-let age:UInt8 = 42
-
-let resultMessage = "Меня зовут " + firstName + ", и мне " + String(age) + " года."
-print("resultMessage -> \(resultMessage)")
-
-
-// task 3
-
-print("Введите первое число.")
-let firstNumber = Int(readLine()!)!
-
-print("Введите второе число.")
-let secondNumber = Int(readLine()!)!
-
-let sumResult = firstNumber + secondNumber
-let sumFirstandSecondNumber = "Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(sumResult) + "."
-print("sumFirstandSecondNumber -> \(sumFirstandSecondNumber)")
-
-
-
-
-// --------------------- string interpolation ---------------
-
-// task_1
-
-let ageTestIntorpolation:UInt8 = 42
-
-let resultMessageInterpolation = "Меня зовут \(firstName), и мне \(age) года."
-print("resultMessageInterpolation -> \(resultMessageInterpolation)")
-
-
-// task_2
-
-print("Введите ваш вес в кг:")
-let weight = Double(readLine()!)!
-
-print("Введите ваш рост в см:")
-let heightCm = Double(readLine()!)!
-
-let heightM = heightCm / 100
-
-let bmi = weight / (heightM * heightM)
-
-print("Ваш ИМТ равен: \(bmi)")
-
-
-
-// task_3
-
-let productName:String = "MacBooK Air"
-let price:Int = 54_000
-let quantity:Int = 22
-
-let totalPrice = "Вы добавили в корзину \(quantity) шт. Товара \(productName) на сумму \(price * quantity) руб"
-
-print("totalPrice -> \(totalPrice)")
-
-
-
-// ----------------- main data types ------------
-
-// task_1
-
-var minIntSixTeen:Int16 = -32_768
-var numberUintEight:UInt8 = 200
-var numberDouble:Double = 3.1415926535
-var isBool:Bool = false
-let message:String = "Боятся нужно только Бога"
-
-
-
-// task_2
-
-print("Пользователь, введите целое число?")
-
-let inputNumber = Int(readLine()!)!
-
-let squareNumber = inputNumber * inputNumber
-print("Результат возведения числа в квадрат равен \(squareNumber)")
-
-
-// task_3
-
-let inputNumberTwo = 42
-let isEven:Bool
-
-isEven = inputNumberTwo % 2 == 0
-print("Введённое число является \(isEven) значением.")
-
-
-
-
-// --------------------- variable and constant -------------------------
-
-// task_1
-
-var temperature:Int8 = 7
-temperature = 10
-
-
-// task_2
-
-let birthYear:Int16 = 1983          // Выбрал данный тип так как было время и до нашей эры а более чем 32 века у нас значительный запас
-//birthYear = 1982                  // константа подразумевает объявление присвоенного значения в переменной единожды
-
-
-// task_3
-
-var count = 10
-//count = "Mobile IOS developer"
-
-/*
- Тут произошло следующее: я могу ошибаться но или компилятор
-  или интерпритатор по умолчанию задаст переменной тип при
-  объявлении без явного присвоения типа как type notation
-  а потом я вмешиваюсь и явно пытаюсь изменить не только значение
-  но и сам тип данных и язык это не допустит
- */
-
-
-
-// ---------------------- if/else ------------------
-
-// task_1
-
-let testNumber = 345_123
-
-if testNumber > 0 {
-    print("Число \(testNumber), является положительным")
-} else if testNumber < 0 {
-    print("Число \(testNumber), является отрицательным")
-} else {
-    print("Число \(testNumber), является нулём")
+struct User {
+    var name: String
+    var surname: String
 }
 
+var userIvan: User = User(name: "Ivan", surname: "Ivanov")
+var userPetr = userIvan
+//userIvan.name = "Petr"
+//userPetr.name = "Petr"
+
+print("userIvan -> \(userIvan)")
+print("userPetr -> \(userPetr)")
 
 
-// task_2
+// Вычисляемые значения
 
-let userAge = 42
 
-if userAge >= 18 {
-    print("Доступ разрешён!")
-} else {
-    print("Доступ запрещён!")
+struct User2 {
+    let firstName: String
+    let lastName: String
+    let birthDate: Date
+    
+    
+    var fullName: String {
+        get {
+            firstName + " " + lastName
+        }
+        set {
+            print("set value \(newValue)")
+        }
+    }
+    
+    // Вычисляемые значения
+    var age: Int {
+        get {
+            Calendar.current.dateComponents([.year], from: birthDate, to: .now).year ?? 0
+        }
+        set {
+            print("Сработал установщик даты set -> \(newValue)")
+        }
+    }
 }
+
+let correctBirthDate: Date = {
+    var components = DateComponents()
+    components.year = 1983
+    components.month = 8
+    components.day = 26
+    return Calendar.current.date(from: components)!
+}()
+
+
+var user: User2 = User2(firstName: "Maksim", lastName: "Minakov", birthDate: correctBirthDate)
+print("user -> \(user)")
+
+print(user.fullName)
+
+print(user.age)
+
+user.fullName = "Petr Petrov"
+
+user.age = 30
+print(user.age)
+
+
+// ------------------------
+
+struct Database {
+    var login: String
+    var isRoot: Bool
+    
+    var isRootUser: Bool {
+        get {
+            isRoot
+        }
+        set {
+            if newValue {
+                if login == "admin", newValue {
+                    isRoot = true
+                } else {
+                    isRoot = false
+                }
+            }
+        }
+    }
+}
+
+var db: Database = Database(login: "admin", isRoot: true)
+db.isRootUser = true
+
+print("db.isRoot -> \(db.isRoot)")
+
+
+
+struct TodoItem {
+    let title: String
+    let isCompleted: Bool
+    
+    var textColor: String {
+        if isCompleted {
+            return "Gray"
+        } else {
+            return "Black"
+        }
+    }
+}
+
+var task = TodoItem(title: "New Book", isCompleted: true)
+print(task.textColor)
+
+
+// - ----------------- Наблюдатели свойств ----------------
+
+struct LoginForm {
+    var email: String = "" {
+        
+        willSet {
+            print("Сейчас будет установлено значение \(newValue)")
+        }
+        
+        didSet {
+            print("Старое значение \(oldValue)")
+            if !email.contains("@") {
+                print("Некоректное значение email: \(email)")
+                isError = true
+            }
+        }
+    }
+    var isError: Bool = false
+}
+
+var form: LoginForm = LoginForm()
+form.email = "sdsd"
+form.email = "maksim.minakov.83@mail.ru"
+
+
+
+
+// 1.
+
+struct Product {
+    let price: Double
+    let discont: Int
+    
+    var priceWithDiscont: Double {
+        price * Double(100 - discont) / 100
+    }
+}
+
+let res = Product(price: 453, discont: 67)
+print(res.priceWithDiscont)
+
+
+
+// 2.
+
+struct Cart {
+    var items: [String] = [] {
+        didSet {
+            totalCount = items.count
+            print(totalCount)
+        }
+    }
+    var totalCount: Int = 0
+    
+    static func doSome() -> Cart {
+        Cart(items: ["1", "2", "3"])
+    }
+}
+
+var count: Cart = Cart()
+count.items = ["0", "1", "2"]
+
+
+// 3. static
+
+var cart: Cart = Cart()
+print(cart)
+
+print(Cart.doSome())
+
+
+// extension
+
+extension String {
+    static let helloText = "Hello"
+    static let appName = "Super app"
+}
+
+print(String.helloText)
+print(String.appName)
