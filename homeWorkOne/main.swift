acaa2993988525eec7ee72c790b3fb65f3c9aa14
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-03 00:24:39 -0800

    homeWorkClass

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,157 +1,173 @@
-class Netvork {
-    var url: String
-    
-    func sendRequest() -> String {
-        return "Data"
-    }
-    
-    init(startUrl: String) {
-        self.url = startUrl
-    }
-}
+// task_1
 
-
-let classNetwork = Netvork(startUrl: "www.google.com")
-
-let newNetwork = classNetwork
-newNetwork.url = "apple.com"
-
-print("newNetwork.url -> \(newNetwork.url)")
-print("classNetwork.url -> \(classNetwork.url)")
-
-
-
-
-// ------------------------------------
-
-class LoginManager {
+class Person {
     var name: String
-    var password: String
-    var email: String
+    var age: Int
     
-    init(name: String, password: String, email: String) {
+    init(name: String, age: Int) {
         self.name = name
-        self.password = password
-        self.email = email
+        self.age = age
+    }
+    
+    func sayHello(){
+        print("Привет, меня зовут \(name)")
     }
     
     
-    func registration(){
-        //
-    }
-    
-    func login(email: String, pswd: String) -> Bool {
-        email == self.email && pswd == self.password
-    }
-}
-
-class RegView {
-    var loginManager = LoginManager(name: "1", password: "2", email: "3")
-    
-    
-    func navToLogin(){
-        _ = LoginView(loginManager: loginManager)
+    // task_3
+    func celebrateBirthday() {
+        age += 1
+        print("С днем рождения, \(name)! Теперь у меня \(age) лет.")
     }
 }
 
-class LoginView {
-    var loginManager: LoginManager
+// person - 1
+let personIvan: Person = Person(name: "Ivan", age: 20)
+personIvan.sayHello()
+
+// person - 2
+let personPetr: Person = Person(name: "Petr", age: 25)
+personPetr.sayHello()
+
+// person - 3
+let personOleg: Person = Person(name: "Oleg", age: 30)
+personOleg.sayHello()
+
+
+// task_2
+
+class Car {
+    var model: String
+    var owner: Person?
     
-    init(loginManager: LoginManager) {
-        self.loginManager = loginManager
+    init(model: String, owner: Person? = nil) {
+        self.model = model
+        self.owner = owner
     }
     
-    func checkLogin(email: String, pswd: String){
-        print(loginManager.login(email: email, pswd: pswd))
+    func assignOwner(_ newOwner: Person) {
+        self.owner = newOwner
+        print("Новый хозяин автомобиля \(model) - \(newOwner.name)")
     }
 }
 
-// -----------------------------------
+// первый владелец - тут создан автомобиль и позже присвоен владелец
+let toyota = Car(model: "Toyota")
+toyota.assignOwner(personIvan)
 
-enum TextFieldType {
-    case normal, custom, fill
+
+// второй владелец сразу присвоен экземпляру автомобиль и владелец и машина
+let bmw = Car(model: "BMW", owner: personPetr)
+bmw.assignOwner(personPetr)
+
+
+// ---- task_3 --- этот метод celebrateBirthday я реализовал в классе Person т.к т.з мне было не совсем ясно
+// и я его реализовал в классе Person без реализации наследования в класс Car, хотя если подумать по
+// названию то этот метод по логике там и не нужен ведь класс отвечает за реализацию автомобиля, наверно так
+
+personIvan.celebrateBirthday()
+personPetr.celebrateBirthday()
+personOleg.celebrateBirthday()
+
+
+
+// task_4
+
+class Animal {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func makeSound() {}
 }
 
-class UITextField {
-    var text: String?
-    var type: TextFieldType
+class Dog: Animal {
+    // task_5
+    var breed: String?
     
-    init(text: String?, type: TextFieldType) {
-        self.text = text
-        self.type = type
+    init(name: String, breed: String? = nil){
+        self.breed = breed
+        super.init(name: name)
     }
     
-    init(type: TextFieldType){
-        self.type = type
+    override func makeSound() {
+        print("Собака \(name) умеет лаить")
+    }
+}
+
+class Cat: Animal {
+    
+    override func makeSound() {
+        print("Кот \(name) умеет мяукать")
+    }
+}
+
+
+let myDog = Dog(name: "Rex")
+let myCat = Cat(name: "Murzic")
+myDog.makeSound()
+myCat.makeSound()
+
+
+// task_5
+
+let dogGermanShepherd: Dog = Dog(name: "Red", breed: "German Shepherd")
+let dogPoodle: Dog = Dog(name: "Марсель", breed: "Poodle")
+
+
+// task_6
+
+class Product {
+    var name: String
+    var price: Double
+    
+    init(name: String, price: Double) {
+        self.name = name
+        self.price = price
+    }
+}
+
+class Store {
+    var products: [Product] = []
+    
+    init(products: [Product]) {
+        self.products = products
     }
     
-    func setupTextField(){
-        switch type {
-        case .normal:
-            print("normall")
-        case .custom:
-            print("custom")
-        case .fill:
-            print("fill")
+    func printCatalog() {
+        for (index, product) in products.enumerated(){
+            print("\(index + 1). \(product.name) - \(product.price) руб")
         }
     }
-}
-
-var textField = UITextField(type: .normal)
-print("textField.type -> \(textField.type)")
-
-
-// 1 - ссылочный тип - экземпляры не копируются а передаются по ссылке
-// 2 - обязательный инит
-// 3 - можем наследовать классы
-
-import Foundation
-
-class Session {
-    var id: String
-    var date: Date
     
-    init(id: String, date: Date) {
-        self.id = id
-        self.date = date
-    }
-    
-    func start(){
-        print("start session \(date)")
-    }
-    
-    func stop(){
-        print("stop session \(date)")
+    func cell(productName: Product){
+        for i in 0..<products.count {
+            if products[i].name == productName.name {
+                    let removedProduct = products.remove(at: i)
+                    print("Товар \(removedProduct.name) удален из корзины")
+                    return
+                }
+            }
+        print("Товар \(productName.name) не найден в корзине")
     }
 }
 
-var mainSession = Session(id: "123", date: Date())
-mainSession.start()
-mainSession.stop()
+// create product
+let milk = Product(name: "milk", price: 90)
+let bread = Product(name: "bread", price: 50)
+let eggs = Product(name: "eggs", price: 89.90)
 
+// add store product
+let store = Store(products: [milk, bread, eggs])
 
-final class CustomSession: Session {
-    
-    var pauseDate: Date
-    
-    init(pauseDate: Date, id: String, date: Date) {
-        // 1. сначала проинициализировать свойства дочернего а потом родителя
-        self.pauseDate = pauseDate
-        
-        // 2. вызови init родителя (id: String, date: Date)
-        super.init(id: id, date: date)
-    }
-    
-    override  func start(){            // переопределение функции в дочернем классе с помощью ключевого слова ovveride
-        print("start session override \(date)")
-    }
-    
-    func pause() {
-        print("pause")
-    }
-}
+// print product price and index
+store.printCatalog()
 
-let customSession = CustomSession(pauseDate: Date(), id: "123", date: Date())
-customSession.start()
-customSession.stop()
-customSession.pause()
+// remove product
+store.cell(productName: milk)
+store.cell(productName: milk)
+
+// final store
+store.printCatalog()
