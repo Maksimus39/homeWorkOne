126619f0fce5c67062e8f5ac1e26f131f04f63dc
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-07 23:11:45 -0800

    homeWorkProtocolQuard

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,228 +1,114 @@
-// Абстракции (ООП)
+// task_1
 
-import Foundation
-
-
-// DI
-
-protocol DataNetworkProtocol: AnyObject {
-    func fetchData(url: String)
+protocol Plugin {
+    func execute()
 }
 
-protocol FileStorageDataNetworkProtocol: DataNetworkProtocol {
-    func uploadData(to url: String)
+class LoggerPlus: Plugin{
+    func execute() {
+        print("Действие выполнено")
+    }
 }
 
-protocol CachDataNetworkProtocol: FileStorageDataNetworkProtocol {
-    func getCacheData(for url: String)
-}
-
-
-class Network: DataNetworkProtocol {
-    
-    var text: String = "Hello world"
-    func fetchData(url: String) {
-        print("Fetching data from \(url)")
+class AnalysticsPlugin: Plugin{
+    func execute() {
+        print("Событие отправлено")
     }
 }
 
 
-class FileStorage: FileStorageDataNetworkProtocol {
-    func uploadData(to url: String) {
-        print("Hello data to \(url)")
+class App{
+   private var plugins: [Plugin] = []
+    
+    func addPlugin(_ plugin: Plugin) {
+        plugins.append(plugin)
     }
     
-    func fetchData(url: String) {
-        print("Hy i`am is company file storage to \(url)")
+    func start() {
+        for plugin in plugins {
+            plugin.execute()
+        }
     }
 }
 
-// 1
-var someData: FileStorageDataNetworkProtocol = FileStorage()
-someData.uploadData(to: "www.google.com")
-someData.fetchData(url: "www.microsoft.com")
+let app = App()
+
+// add plugin
+app.addPlugin(LoggerPlus())
+app.addPlugin(AnalysticsPlugin())
+
+// plugin start
+app.start()
 
 
-// 2
+// task_2
 
-var datas: [DataNetworkProtocol] = [Network(), FileStorage()]
-//print("datas -> \(datas)")
-
-for data in datas {
-    data.fetchData(url: "www.apple.com ->")
+protocol NetworkServiceProtocol {
+    func fetchData() -> String
 }
 
-
-class Model {
-    
-    var network: DataNetworkProtocol?
-    
-    init(network: DataNetworkProtocol) {
-        self.network = network
-    }
-    
-    func sendRequest() {
-        network?.fetchData(url: "https://example.com")
+class RealNetworkService: NetworkServiceProtocol {
+    func fetchData() -> String {
+        return "Data from network"
     }
 }
 
-
-let network = Network()
-let mockModel = Model(network: network)
-mockModel.sendRequest()
-
-
-
-// primer iz ui
-
-protocol ReloadTableProtocolo {
-    func reloadTable()
+class MockNetworkService: NetworkServiceProtocol {
+    func fetchData() -> String {
+        return "Data from mock"
+    }
 }
 
-protocol SetConstraintProtocol {
-    func setConstraint()
-}
 
-typealias CombinedProtocol = ReloadTableProtocolo & SetConstraintProtocol
-
-class ViewController: CombinedProtocol {
-   
-    func reloadTable() {
-        print("reloadTable")
+class ViewModel {
+    private var networkService: NetworkServiceProtocol
+    
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
     }
     
-    func setConstraint() {
-        print("setConstraint")
-    }
-}
-
-let res: CombinedProtocol = ViewController()
-res.reloadTable()
-res.setConstraint()
-
-
-// VIPER
-// V - View
-// I - iteractor
-// P - Presenter
-// E - Entities
-// R - Router
-
-
-
-
-// -------------------- EXTENSION -----------------------------
-
-protocol ConnnectProtocol {
-    func startConnection()
-}
-
-extension ConnnectProtocol {
-    func startConnection() {
-        print("startConnection protocol!!!")
-    }
-}
-
-class User: ConnnectProtocol {
-    func startConnection() {
-        print("startConnection protocol!!! from class!!!")
-    }
-}
-
-let user = User()
-user.startConnection()
-
-
-
-// -------------- standart protocol -------------
-
-struct UserEquitable: Equatable, Comparable {
-    static func < (lhs: UserEquitable, rhs: UserEquitable) -> Bool {
-        lhs.age < rhs.age
-    }
-    
-   
-    var name: String
-    var age: Int
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
-}
-
-var userOne = UserEquitable(name: "Maksim", age: 42)
-var userTwo = UserEquitable(name: "Maksim", age: 39)
-
-
-print(userOne == userTwo)
-print(userOne > userTwo)
-
-
-// Hachable
-
-struct KeyType: Hashable {
-    var name: String
-    
-    init(name: String) {
-        self.name = name
-    }
-}
-
-var soveDic: [KeyType: String]
-
-
-
-// protocol Identifiable
-
-struct Item: Identifiable {
-    var id: String = UUID().uuidString
-    var name: String
-    var age: String
-}
-
-
-var item = Item(name: "Maksim", age: "42")
-print("item -> \(item)")
-
-
-
-// guard
-
-class NetworkManager {
-    func fetchData(url: String?, data: Data?){
-        
-        guard url != nil else {return}
-        //
-        //
-        //
-        //
-        //
-        guard data != nil else {return}
-        //
-        //
-        //
-        //
+    func loadData() -> String {
+        return networkService.fetchData()
     }
 }
 
 
-// primer
+let realService = RealNetworkService()
+let mockService = MockNetworkService()
 
-func calculate(number: Int?) -> Int {
-    if let number {
-        return number * 2
+let viewModelWithRealService = ViewModel(networkService: realService)
+let viewModelWithMockService = ViewModel(networkService: mockService)
+
+print(viewModelWithRealService.loadData())
+print(viewModelWithMockService.loadData())
+
+
+// task_3
+
+protocol Movable {
+    func moveForward()
+    func moveBackward()
+}
+
+protocol Flyable: Movable {
+    func fly()
+}
+
+typealias Combyne = Movable & Flyable
+
+class Bird: Combyne {
+    func moveForward() {
+        print("Птица пошла вперёд")
     }
-    return 0
+    func moveBackward() {
+        print("Птица пошла назад")
+    }
+    func fly() {
+        print("Птица полетела")
+    }
 }
 
-print(calculate(number: 4))
-
-
-func calculate2(number: Int?) -> Int {
-    guard let number else {return 0}
-    return number * 2
-}
-
-
-print(calculate2(number: 4))
+let bird = Bird()
+bird.moveForward()
+bird.moveBackward()
+bird.fly()
