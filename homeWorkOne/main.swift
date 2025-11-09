8cfeeb6b9d63dde203db502831d327731d6a0dcf
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-08 22:05:13 -0800

    learn closure

--- homeWorkOne/main.swift
+++ homeWorkOne/main.swift
@@ -1,160 +1,86 @@
-// ------------------ Concatenation --------------
+// closure замыкания лямбда функции
 
-// task_1
+func doSome(){
+    print("Hello")
+}
 
-let firstName:String = "Максим"
-let lastName:String = "Минаков"
-let fullName:String = firstName + " " + lastName
+let a = doSome
+a()
 
-print("fullName -> \(fullName)")
 
+//
 
+func calc(num: Int) -> Int {
+    return num + num
+}
 
-// task_2
+var a2 = calc(num: 4)
+print(a2)
 
-let age:UInt8 = 42
 
-let resultMessage = "Меня зовут " + firstName + ", и мне " + String(age) + " года."
-print("resultMessage -> \(resultMessage)")
+// v1
+var calVal: (Int) -> Int = { num in
+    return num * 100
+}
 
+// v2
+var calVal2: (Int) -> Int = { num in
+    num * 100
+}
 
-// task 3
+// 3
 
-print("Введите первое число.")
-let firstNumber = Int(readLine()!)!
+var calVal3: (Int) -> Int = { $0 * $0 }
 
-print("Введите второе число.")
-let secondNumber = Int(readLine()!)!
 
-let sumResult = firstNumber + secondNumber
-let sumFirstandSecondNumber = "Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(sumResult) + "."
-print("sumFirstandSecondNumber -> \(sumFirstandSecondNumber)")
+//print(calVal(5))
 
+// 4
 
+var calVal4: (Int) -> Int = { $0 * $0 }
 
+func test(closure: (Int) -> Int){
+    print(closure(200))
+}
 
-// --------------------- string interpolation ---------------
+test(closure: calVal3)
 
-// task_1
+test { num in
+    let random = Int.random(in: 1...100)
+    return num * random
+}
 
-let ageTestIntorpolation:UInt8 = 42
+func closureName(name: String, closure: (Int) -> Int){
+    let random = Int.random(in: 0...100)
+    print(name.uppercased(), closure(random))
+}
 
-let resultMessageInterpolation = "Меня зовут \(firstName), и мне \(age) года."
-print("resultMessageInterpolation -> \(resultMessageInterpolation)")
-
-
-// task_2
-
-print("Введите ваш вес в кг:")
-let weight = Double(readLine()!)!
-
-print("Введите ваш рост в см:")
-let heightCm = Double(readLine()!)!
-
-let heightM = heightCm / 100
-
-let bmi = weight / (heightM * heightM)
-
-print("Ваш ИМТ равен: \(bmi)")
-
-
-
-// task_3
-
-let productName:String = "MacBooK Air"
-let price:Int = 54_000
-let quantity:Int = 22
-
-let totalPrice = "Вы добавили в корзину \(quantity) шт. Товара \(productName) на сумму \(price * quantity) руб"
-
-print("totalPrice -> \(totalPrice)")
-
-
-
-// ----------------- main data types ------------
-
-// task_1
-
-var minIntSixTeen:Int16 = -32_768
-var numberUintEight:UInt8 = 200
-var numberDouble:Double = 3.1415926535
-var isBool:Bool = false
-let message:String = "Боятся нужно только Бога"
-
-
-
-// task_2
-
-print("Пользователь, введите целое число?")
-
-let inputNumber = Int(readLine()!)!
-
-let squareNumber = inputNumber * inputNumber
-print("Результат возведения числа в квадрат равен \(squareNumber)")
-
-
-// task_3
-
-let inputNumberTwo = 42
-let isEven:Bool
-
-isEven = inputNumberTwo % 2 == 0
-print("Введённое число является \(isEven) значением.")
-
-
-
-
-// --------------------- variable and constant -------------------------
-
-// task_1
-
-var temperature:Int8 = 7
-temperature = 10
-
-
-// task_2
-
-let birthYear:Int16 = 1983          // Выбрал данный тип так как было время и до нашей эры а более чем 32 века у нас значительный запас
-//birthYear = 1982                  // константа подразумевает объявление присвоенного значения в переменной единожды
-
-
-// task_3
-
-var count = 10
-//count = "Mobile IOS developer"
-
-/*
- Тут произошло следующее: я могу ошибаться но или компилятор
-  или интерпритатор по умолчанию задаст переменной тип при
-  объявлении без явного присвоения типа как type notation
-  а потом я вмешиваюсь и явно пытаюсь изменить не только значение
-  но и сам тип данных и язык это не допустит
- */
-
-
-
-// ---------------------- if/else ------------------
-
-// task_1
-
-let testNumber = 345_123
-
-if testNumber > 0 {
-    print("Число \(testNumber), является положительным")
-} else if testNumber < 0 {
-    print("Число \(testNumber), является отрицательным")
-} else {
-    print("Число \(testNumber), является нулём")
+closureName(name: "Maksim") { num in
+    return num * 2
 }
 
 
 
-// task_2
+// primer
 
-let userAge = 42
-
-if userAge >= 18 {
-    print("Доступ разрешён!")
-} else {
-    print("Доступ запрещён!")
+func chesIsOdd(number: [Int], completionHandler: (Int) -> Bool) -> [Int] {
+    var numPlus: [Int] = []
+    
+    number.forEach { i in
+        if completionHandler(i) {
+            numPlus.append(i)
+        }
+    }
+    return numPlus
 }
+
+var numArr = [1,2,3,4,5,6,7,8,9,10]
+numArr.sort { $0 > $1 }
+
+let resultPlusArray = chesIsOdd(number: numArr, completionHandler: { $0 % 2 == 0 })
+let resultMinusArray = chesIsOdd(number: numArr, completionHandler: { $0 % 2 != 0 })
+
+
+print("resultPlusArray -> \(resultPlusArray)")
+print("resultMinusArray -> \(resultMinusArray)")
+print(numArr)
